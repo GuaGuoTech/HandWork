1e41896ead8b34c418f277dedf54e086f29accea
Winter <924756570@qq.com>
2016-03-29 17:28:44 +0800

    HtmlHelper

--- GuaguoHtmlHelper/App_Start/RouteConfig.cs
+++ GuaguoHtmlHelper/App_Start/RouteConfig.cs
@@ -5,17 +5,18 @@ using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
 
-namespace HandWork.Com.MVC
+namespace GuaguoHtmlHelper
 {
     public class RouteConfig
     {
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
             routes.MapRoute(
                 name: "Default",
-                url: "{modules}/{controller}/{action}/{id}",
-                defaults: new { modules = "Homes", controller = "Home", action = "Index", id = UrlParameter.Optional }
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
