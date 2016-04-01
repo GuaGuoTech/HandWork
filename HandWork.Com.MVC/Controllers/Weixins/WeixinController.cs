e7c2317bd0d3ed2acf67da1c28a08e9606655f7e
WinterWan <wantaotao1994@gmail.com>
2016-04-01 11:49:01 +0800

    微信接口对接

---
+++ HandWork.Com.MVC/Controllers/Weixins/WeixinController.cs
@@ -0,0 +1,44 @@
+﻿using HandWork.Com.Service.Weixins;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HandWork.Com.MVC.Controllers.Weixins
+{
+    public class WeixinController : Controller
+    {
+        //
+        // GET: /Weixin/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public string CheckDevelopment()
+        {
+            string token = "guaguokeji";
+            string signature = Request.Params["signature"];
+            string timestamp = Request.Params["timestamp"];
+            string nonce = Request.Params["nonce"];
+            string echostr = Request.Params["echostr"];
+
+
+
+            string[] arr = { token, timestamp, nonce };
+            Array.Sort(arr);
+
+            string sunStr = arr[0].ToString().Trim() + arr[1].ToString().Trim() + arr[2].ToString().Trim();
+
+            if (WeixinService.GetPwd(sunStr).ToLower() == signature)
+            {
+                return echostr; 
+
+            }
+
+            return null;
+        }
+    }
+}
---
+++ HandWork.Com.Service/Weixins/WeixinService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HandWork.Com.Service.Weixins
+{
+    public class WeixinService
+    {
+        /// <summary>
+        /// sha1加密
+        /// </summary>
+        /// <param name="Pwd"></param>
+        /// <returns></returns>
+     public   static string GetPwd(string Pwd)
+        {
+            byte[] data = System.Text.Encoding.Default.GetBytes(Pwd);//以字节方式存储
+            System.Security.Cryptography.SHA1 sha1 = new System.Security.Cryptography.SHA1CryptoServiceProvider();
+            byte[] result = sha1.ComputeHash(data);//得到哈希值
+            return System.BitConverter.ToString(result).Replace("-", ""); //转换成为字符串的显示
+        }
+
+
+    }
+}
