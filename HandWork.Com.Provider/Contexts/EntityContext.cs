bd1c5007c358853df5a631fc7a302df5f054a431
DeAllen <1551939904@qq.com>
2016-04-05 15:07:44 +0800

    dfsd

--- HandWork.Com.Provider/Contexts/EntityContext.cs
+++ HandWork.Com.Provider/Contexts/EntityContext.cs
@@ -7,14 +7,15 @@ using HandWork.Com.Model.Dealers;
 using HandWork.Com.Model.Workers;
 using HandWork.Com.Model.Employment;
 using HandWork.Com.Model.Recruits;
+using HandWork.Com.Model.Users;
 
 namespace HandWork.Com.Provider.Contexts
 {
     public class EntityContext : DbContext
     {
         public EntityContext() : base("name=EntityContext") { }
-        public DbSet<Dealer> Dealers { get; set; }
-        public DbSet<Worker> Workers { get; set; }
+        public DbSet<User> Users { get; set; }
+        //public DbSet<User> Users { get; set; }
         public DbSet<Employment> Employments { get; set; }
         public DbSet<Recruit> Recruits { get; set; }
 
