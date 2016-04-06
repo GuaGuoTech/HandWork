eff4a04d55203ffabe3017f34b8e7a5a2f79e4a0
Winter <924756570@qq.com>
2016-04-06 13:40:58 +0800

    增加user页面

--- HandWork.Com.MVC/Controllers/Homes/HomeController.cs
+++ HandWork.Com.MVC/Controllers/Homes/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using HandWork.Com.Service.Weixins;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -10,10 +11,25 @@ namespace HandWork.Com.MVC.Controllers.Home
     {
         //
         // GET: /Home/
+        readonly log4net.ILog logger = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
 
         public ActionResult Index()
         {
-            return View();
+            string code = Request.Params["code"];
+            if (code != null)
+            {
+
+                string url = string.Format("https://api.weixin.qq.com/sns/oauth2/access_token?appid=wxd21f90079ecb0969&secret=338345d734124088ce5579a6a4514318&code={0}&grant_type=authorization_code", code);
+
+                string responseText = WeixinService.HttpsGet(url);
+                ///日志记录
+                logger.Info(responseText);
+
+
+                return View(WeixinService.GetWeixinUser(responseText));
+
+            }
+            return Json("erro",JsonRequestBehavior.AllowGet);
         }
 
     }
