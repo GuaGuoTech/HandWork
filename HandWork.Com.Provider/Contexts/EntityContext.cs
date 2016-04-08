20eceaacdec9b6746e6af254ff01f21472a78494
DeAllen <1551939904@qq.com>
2016-04-08 15:29:25 +0800

    Services 测试可用

--- HandWork.Com.Provider/Contexts/EntityContext.cs
+++ HandWork.Com.Provider/Contexts/EntityContext.cs
@@ -3,9 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Data.Entity;
 using System.Web;
-using HandWork.Com.Model.Dealers;
-using HandWork.Com.Model.Workers;
-using HandWork.Com.Model.Employment;
+//using HandWork.Com.Model.Dealers;
+//using HandWork.Com.Model.Workers;
+using HandWork.Com.Model.WantedJobs;
 using HandWork.Com.Model.Recruits;
 using HandWork.Com.Model.Users;
 
@@ -16,7 +16,7 @@ namespace HandWork.Com.Provider.Contexts
         public EntityContext() : base("name=EntityContext") { }
         public DbSet<User> Users { get; set; }
         //public DbSet<User> Users { get; set; }
-        public DbSet<Employment> Employments { get; set; }
+        public DbSet<WantedJob> WantedJobs { get; set; }
         public DbSet<Recruit> Recruits { get; set; }
 
 
