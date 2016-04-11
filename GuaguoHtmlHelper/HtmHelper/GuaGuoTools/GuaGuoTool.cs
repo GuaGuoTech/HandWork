60d80ddbb759bcba22129429b24275770c1277a8
Winter <924756570@qq.com>
2016-04-11 18:54:35 +0800

    增加一个扩展空间

---
+++ GuaguoHtmlHelper/HtmHelper/GuaGuoTools/GuaGuoTool.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace System.Web.Mvc
+{
+    public static class GuaGuoTool
+    {
+        public static HtmlString GuaGuo_PhoneNumberCheck(this HtmlHelper htmlHelper)
+        {
+            string phoneCheck = "<div class=\"form-group\"><label>电话号码：</label><div class=\"input-group\"><input type=\"text\" class=\"form-control\" id=\"_phoneNumber\" placeholder=\"电话号码\"><div class=\"input-group-addon btn btn-primary\">发送验证码</div></div><div id=\"phone_info\"></div></div>";
+            return new HtmlString(phoneCheck);
+        }
+    }
+}
