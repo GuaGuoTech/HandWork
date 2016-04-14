eb0849417d6aa0accd51791b0dca6d07ab656bc5
MissYangIce <879492549@qq.com>
2016-04-14 17:18:05 +0800

    发布和详情页 前台样式

---
+++ HandWork.Com.MVC/Controllers/Details/DetailController.cs
@@ -0,0 +1,29 @@
+﻿using HandWork.Com.Model.Recruits;
+using HandWork.Com.Provider.Contexts;
+using HandWork.Com.Provider.Repositorys;
+using HandWork.Com.Service.Recruits;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HandWork.Com.MVC.Controllers.Recruits
+{
+    public class DetailController : Controller
+    {
+        Repository<Recruit> repo = new Repository<Recruit>(new EntityContext());
+        public ActionResult Index()
+        {
+            //if (RecruitService.CheckConfirm(id) == 0)
+            //{
+            //    //Response.("");
+            //    Redirect("~/Users/User/Index.cshtml");
+            //}
+            return View();
+
+        }
+
+        public long id { get; set; }
+    }
+}
