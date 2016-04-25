093ea85438c3ec149bf6dee548877e30fe9b0497
MikeChen <1551939904@qq.com>
2016-04-25 14:55:18 +0800

    更改了Recruit的Model 增加字段

--- HandWork.Com.Model/Recruits/Recruit.cs
+++ HandWork.Com.Model/Recruits/Recruit.cs
@@ -21,10 +21,18 @@ namespace HandWork.Com.Model.Recruits
         public long Id { get; set; }
 
         /// <summary>
+        /// 当前招工的发布者
+        /// </summary>
+        [Column("man", TypeName = "NVARCHAR")]
+        public string Man { get; set; }
+        
+
+        /// <summary>
         /// 设置对象的地址
         /// </summary>
         [Column("location", TypeName = "NVARCHAR")]
         public string Location { get; set; }
+
 
         /// <summary>
         /// 设置佣金比例  
@@ -55,19 +63,36 @@ namespace HandWork.Com.Model.Recruits
         /// 当前对象身份证号码
         /// </summary>
         [Column("sfz_account", TypeName = "VARCHAR")]
-        public string SfzAccount { get; set; }  
+        public string SfzAccount { get; set; }
 
         /// <summary>
-        /// 当前招工的简介
+        /// 当前招工的简介关键词 
+        /// </summary>
+        [Column("words", TypeName = "VARCHAR")]
+        public string Words { get; set; }
+        /// <summary>
+        /// 当前招工的详情介绍
         /// </summary>
         [Column("note", TypeName = "NVARCHAR")]
         public string Note { get; set; }
+
+        /// <summary>
+        /// 当前招工的详情介绍
+        /// </summary>
+        [Column("requirement", TypeName = "NVARCHAR")]
+        public string Requirement { get; set; }
         
         /// <summary>
         /// 当前对象性别 0女1男2不限
         /// </summary>
         [Column("sex", TypeName = "INT")]
         public int Sex { get; set; }
+
+        /// <summary>
+        /// 设置结算方式 0日结 1周结 2月结 3其他 
+        /// </summary>
+        [Column("way", TypeName = "INT")]
+        public int Way { get; set; }
 
         /// <summary>
         /// 设置当前薪金
@@ -88,6 +113,21 @@ namespace HandWork.Com.Model.Recruits
         /// </summary>
         [Column("first_time", TypeName = "DATETIME")]
         public DateTime FirstTime { get; set; }
+
+        /// <summary>
+        /// 设置工作日期
+        /// </summary>
+        [Column("work_date", TypeName = "DATETIME")]
+        public DateTime WorkDate { get; set; }
+
+        /// <summary>
+        /// 设置工作时间
+        /// </summary>
+        [Column("work_time", TypeName = "DATETIME")]
+        public DateTime WorkTime { get; set; }
+
+
+
 
         /// <summary>
         /// 当前是否人工确认
